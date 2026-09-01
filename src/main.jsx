394040c9829330e1d38263d4f3a19a6ff8f0c246
Mariana Rosa <marianarsamaral@gmail.com>
2026-09-01 18:52:54 -0300

    import do BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,20 @@ import { Login } from './pages/Login/index.jsx'
 import { Register } from './pages/Register/index.jsx'
 import { Feed } from './pages/Feed/index.jsx'
 import { BlogPost } from './pages/BlogPost/index.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
-    {/* <Register /> */}
-    {/* <Feed /> */}
-    {/* <BlogPost /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth/register' element={<Register />} />
+        <Route path='/auth/login' element={<Register />}></Route>
+          
+      <Routes/>
+      {/* <Login /> */}
+      {/* <Register /> */}
+      {/* <Feed /> */}
+      {/* <BlogPost /> */}
+    </BrowserRouter>
   </StrictMode>,
 )
